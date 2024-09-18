ea372ddb017c14a6f32085979350c15bb1e27dea
joshua <fullstack5451@gmail.com>
2024-09-17 16:44:16 -1200

    nuevos ejercicios utilizando For

---
+++ actividadFor3/src/ejercicioFor3/ejemploFor3.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicioFor3;
+
+
+public class ejemploFor3 {
+    public static void main(String [] args){
+        for (int i = 5; 5 <= 50; i+=5){
+            System.out.println(i);
+        }
+    }
+    
+}
